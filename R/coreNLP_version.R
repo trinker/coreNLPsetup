0fc06d43b17e576bb56f5ffde78446a703a49192
Tyler <tyler.rinker@gmail.com>
2017-09-11 23:43:24 -0400

    upped stanford version

--- R/coreNLP_version.R
+++ R/coreNLP_version.R
@@ -6,6 +6,6 @@
 #' @references \url{http://stanfordnlp.github.io/CoreNLP/index.html#download}
 #' @examples
 #' coreNLP_version
-coreNLP_version <- 'stanford-corenlp-full-2016-10-31'
+coreNLP_version <- 'stanford-corenlp-full-2017-06-09'
 
 
